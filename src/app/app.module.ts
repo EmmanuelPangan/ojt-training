3037ee4e6a3f65c48c2b0c36469f1c5e90fe63f9
BinKei-2200459 <2200459@usl.edu.ph>
2026-02-02 22:36:40 +0800

    Create header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AvailabilityStatusPipe } from './availability-status.pipe';
+import { HeadComponent } from './head/head.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AvailabilityStatusPipe
+    AvailabilityStatusPipe,
+    HeadComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule
