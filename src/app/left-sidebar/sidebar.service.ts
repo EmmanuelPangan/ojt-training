6b0a51e8528562d7ec4d185480e685887635bd95
BinKei-2200459 <2200459@usl.edu.ph>
2026-02-04 11:20:56 +0800

    Route 02

---
+++ src/app/left-sidebar/sidebar.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class SidebarService {
+  private _open = new BehaviorSubject<boolean>(false);
+  isOpen$ = this._open.asObservable();
+
+  toggle() {
+    this._open.next(!this._open.value);
+  }
+
+  close() {
+    this._open.next(false);
+  }
+}
